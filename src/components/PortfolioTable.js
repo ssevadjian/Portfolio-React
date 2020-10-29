5af4a53972f21e01e3ccee0f6cbc5149a5e59040
SJS <sjsevadjian@gmail.com>
2020-10-29 00:25:40 -0700

    updated table in PortfolioTable.js

--- src/components/PortfolioTable.js
+++ src/components/PortfolioTable.js
@@ -1,61 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import "materialize-css";
 import { Table } from "react-materialize";
 
-class MyTable {
-  render() {
+export default function MyTable() {
     return (
       <Table>
       <thead>
         <tr>
           <th data-field="id">
-            Name
-          </th>
-          <th data-field="name">
-            Item Name
-          </th>
-          <th data-field="price">
-            Item Price
+            Portfolio
           </th>
         </tr>
       </thead>
-      <tbody>
-        <tr>
-          <td>
-            Alvin
-          </td>
-          <td>
-            Eclair
-          </td>
-          <td>
-            $0.87
-          </td>
-        </tr>
-        <tr>
-          <td>
-            Alan
-          </td>
-          <td>
-            Jellybean
-          </td>
-          <td>
-            $3.76
-          </td>
-        </tr>
-        <tr>
-          <td>
-            Jonathan
-          </td>
-          <td>
-            Lollipop
-          </td>
-          <td>
-            $7.00
-          </td>
-        </tr>
-      </tbody>
     </Table>
     )
   }
-}
 
