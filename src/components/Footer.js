1278639272b9569db19e1b74158c9d99b9b069c6
SJS <sjsevadjian@gmail.com>
2020-10-31 20:55:52 -0700

    fixed footer; added content to AboutMe

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,94 +1,26 @@
 import React from "react";
 import "./footer.css";
-import Particles from "react-tsparticles";
-import BottomNavigation from "@material-ui/core/BottomNavigation";
-import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-import RestoreIcon from "@material-ui/icons/Restore";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import LocationOnIcon from "@material-ui/icons/LocationOn";
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-  root: {
-    width: 500,
-  },
-});
 
 export default function Footer() {
-  // const classes = useStyles();
-  // const [value, setValue] = React.useState(0);
   return (
     <div class="card-footer text-muted">
-      <ion-icon name="logo-facebook"></ion-icon>
-    2 days ago
+    <footer class="footer mt-auto py-3">
+    <div class="container">
+      <div class="text-muted">
+        <div class="d-flex row justify-content-center">
+          <div class="nav-item col-4">
+            <a href="https://www.linkedin.com/in/stephanie-sevadjian-a3293aa4" target="_blank" >LinkedIn</a>
+          </div>
+          <div class="nav-item col-4">
+            <a href="https://twitter.com/SSevadjian" target="_blank">Twitter</a>
+          </div>
+          <div class="nav-item col-4">
+            <a href="https://www.facebook.com/stephanie.sevadjian" target="_blank">Facebook</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
   </div>
-    // <div className="footer">
-    //   <BottomNavigation
-    //     value={value}
-    //     onChange={(event, newValue) => {
-    //       setValue(newValue);
-    //     }}
-    //     showLabels
-    //     className={classes.root}
-    //   >
-    //     <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-    //     <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-    //     <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-    //     <Particles
-    //       options={{
-    //         interactivity: {
-    //           detectsOn: "canvas",
-    //           events: {
-    //             onClick: {
-    //               enable: true,
-    //               mode: "push",
-    //             },
-    //             onHover: {
-    //               enable: true,
-    //               mode: "repulse",
-    //             },
-    //             resize: true,
-    //           },
-    //           modes: {
-    //             bubble: {
-    //               distance: 400,
-    //               duration: 2,
-    //               opacity: 0.8,
-    //               size: 40,
-    //             },
-    //             push: {
-    //               quantity: 4,
-    //             },
-    //             repulse: {
-    //               distance: 200,
-    //               duration: 0.4,
-    //             },
-    //           },
-    //         },
-    //         particles: {
-    //           color: {
-    //             value: "#3cd1c2",
-    //           },
-    //           collisions: {
-    //             enable: true,
-    //           },
-    //           move: {
-    //             direction: "none",
-    //             enable: true,
-    //             outMode: "bounce",
-    //             random: false,
-    //             speed: 1,
-    //             straight: false,
-    //           },
-    //           size: {
-    //             random: true,
-    //             value: 2,
-    //           },
-    //           detectRetina: true,
-    //         },
-    //       }}
-    //     />
-    //   </BottomNavigation>
-    // </div>
   );
 }
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Container } from "@material-ui/core";
 import { Image, Col, Row } from "react-bootstrap";
+import './../styles/style.css';
+
 export default function AboutMe() {
     return (
-        <div>
+        <div class="aboutBg">
             <Container>
                 <Row>
-                    <Col lg={5}>
-                        
+                    <Col lg={4}>
+                    <div class="mx-auto">
+                    <img class="img-thumbnail img-fluid aboutImg rounded-circle float-right" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/90261217_10158014546514030_4166488760998952960_o.jpg?_nc_cat=101&ccb=2&_nc_sid=09cbfe&_nc_ohc=6BzCYXR9tvMAX8I14Pk&_nc_ht=scontent-sjc3-1.xx&oh=9ebd370bdc467b4676ffdc3dffede9ac&oe=5FC48433" alt="my picture"/>
+                    </div>
+                    </Col>
+                    <Col lg={6}>
+                        <p class='aboutTxt'>
+                            I am a 33 year old front-end and back-end web developer based in San Francisco. I have experience creating interactive applications and working with databases using JavaScript, Express, mySql, node, and React. This site was built in React with Bootstrap and Material-UI.
+                        </p>
+                        <p class="aboutTxt">
+                            My main goal is to create beautiful web applications, mainly focused on the back-end.
+                        </p>
                     </Col>
                 </Row>
             </Container>
