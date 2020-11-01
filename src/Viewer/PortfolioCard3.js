0077096187bf21579e82db16a9ed730f84635ffe
SJS <sjsevadjian@gmail.com>
2020-10-31 21:27:28 -0700

    created more cards to display projects

---
+++ src/Viewer/PortfolioCard3.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import groupProject1 from '../../src/images/groupProject1image.png';
+import './portfolioCardMedia.css';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 220,
+  },
+});
+
+export default function MediaCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          component="img"
+          height="140"
+          image={groupProject1}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Dinner and a Movie
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Some text....
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,39 @@
+import { Container } from '@material-ui/core';
 import React from 'react';
-import PortfolioCards from './../Viewer/PortfolioView';
+import {Row, Col} from 'react-bootstrap';
+import PortfolioCard1 from '../Viewer/PortfolioCard1';
+import PortfolioCard2 from '../Viewer/PortfolioCard2';
+import PortfolioCard3 from '../Viewer/PortfolioCard3';
+import PortfolioCard4 from '../Viewer/PortfolioCard4';
 
 export default function Portfolio() {
     return (
-        <h1>Stephanie Sevadjian</h1>,
-        <PortfolioCards/>
+        <div class="projectImg">
+        <Container>
+        <Row>
+            <Col lg={1}></Col>
+            <Col lg={5}>
+            <PortfolioCard1/>
+            </Col>
+            <Col lg={1}></Col>
+            <Col lg={5}>
+            <PortfolioCard2/>
+            </Col>
+            <Col lg={1}></Col>
+        </Row>
+        <div id="#div"><br></br><br></br></div>
+        <Row>
+            <Col lg={1}></Col>
+            <Col lg={5}>
+            <PortfolioCard3/>
+            </Col>
+            <Col lg={1}></Col>
+            <Col lg={5}>
+            <PortfolioCard4/>
+            </Col>
+            <Col lg={1}></Col>
+        </Row>
+        </Container>
+        </div>
     )
 }
